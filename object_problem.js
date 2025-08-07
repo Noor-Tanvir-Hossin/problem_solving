29c80cff30054c2fa59ceb17d09315576a2a0315
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-08-07 22:27:32 +0600

    transform an array of object into a single object by key

--- object_problem.js
+++ object_problem.js
@@ -1,24 +1,37 @@
-// How do you find the key with the highest value in an object?
+// // How do you find the key with the highest value in an object?
 
-function findTopScorer(scores) {
-    let max = -Infinity;
-    let topScorer = null;
+// function findTopScorer(scores) {
+//     let max = -Infinity;
+//     let topScorer = null;
   
-    for (const name in scores) {
-      if (scores[name] > max) {
-        max = scores[name];
-        topScorer = name;
-      }
-    }
+//     for (const name in scores) {
+//       if (scores[name] > max) {
+//         max = scores[name];
+//         topScorer = name;
+//       }
+//     }
   
-    return topScorer;
-  }
+//     return topScorer;
+//   }
   
-  const scores = {
-    Tanvir: 70,
-    Robin: 95,
-    Noor: 85
-  };
+//   const scores = {
+//     Tanvir: 70,
+//     Robin: 95,
+//     Noor: 85
+//   };
   
-  console.log(findTopScorer(scores)); 
+//   console.log(findTopScorer(scores)); 
   
+// -------- How do you transform an array of objects into a single object by key? -----------
+
+const users = [
+  { id: 1, name: "Tanvir" },
+  { id: 2, name: "Robin" }
+];
+
+const result = users.reduce((acc, user) => {
+  acc[user.id] = user.name;
+  return acc;
+}, {});
+
+console.log(result); 
